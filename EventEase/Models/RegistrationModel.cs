4a1bbffa5fe58129b545571dc7e3268bdfed564f
MaazAsif95 <maazasif@gmail.com>
2026-03-16 15:15:06 +0500

    changes for registration validatiojns

--- EventEase/Models/RegistrationModel.cs
+++ EventEase/Models/RegistrationModel.cs
@@ -1,12 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventEase.Models
 {
     public class RegistrationModel
     {
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
         public string Email { get; set; } = string.Empty;
+
         public DateTime RegisteredAt { get; set; }
 
         // Attendance tracking
